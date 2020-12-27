d2b8ec911f4e72c4da137b34d69c63f252bfafc0
cravler <sergeizz@gmail.com>
2020-12-27 21:48:09 +0200

    app store improvements

---
+++ assets/js/store.js
@@ -0,0 +1,20 @@
+var APP_STORE = [
+    {
+        appid: 'tv_app_store',
+        name: 'TV App Store',
+        url: window.location.origin,
+        text: 'The smallest Hisense TV app store in the world. Offers the most sought apps, that not found in the TV store.'
+    },
+    {
+        appid: 'full_screen_browser',
+        name: 'Full Screen Browser',
+        url: window.location.origin + '/browser.html',
+        text: 'Web browser that allows for full screen viewing.'
+    },
+    {
+        appid: 'ott_player',
+        name: 'Ott Player',
+        url: 'http://widget.ottplayer.es/',
+        text: 'Ott Player - a service that allows you to collect all your IP-TV in one playlist, adjust the channel order, receive electronic program guide.'
+    }
+];
